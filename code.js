3993f995205de8658ced7cc5fa0470f51e5b03cd
Nicholas-py <nicholas.waslander@gmail.com>
2024-12-26 14:33:35 -0500

    Fix CSS

--- code.js
+++ code.js
@@ -8,14 +8,5 @@ function setup() {
 }
 
 function incodefunc() {
-    if (start == null){
-        document.getElementById("toptexty").innerHTML = strhihi+"97";
-}
-else {
-    document.getElementById("toptexty").innerHTML = "lolfail";
-}
-    var oldtext = start.ariaAtomic +"no errors";
-    document.getElementById("toptexty").innerHTML = oldtext;
-    start.innerHTML = "burp";
     start.innerHTML = inp.value;
 }

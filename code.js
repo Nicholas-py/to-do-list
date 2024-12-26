4ea8ceb6930cbc837dd5617afc54eaa1366b7557
Nicholas-py <nicholas.waslander@gmail.com>
2024-12-26 18:40:26 -0500

    Using localstorage

--- code.js
+++ code.js
@@ -1,12 +1,15 @@
 
-var start, inp, strhihi;
+var start, inp;
 
+var oops;
 function setup() {
     start = document.getElementById("toptexty");
     inp = document.getElementById("first");
-    strhihi = "that was a bad idea";
+    oops = [1,2,3,4];
+    start.innerText = localStorage.getItem("Header");
 }
 
 function incodefunc() {
-    start.innerHTML = inp.value;
+    start.innerText = inp.value;
+    localStorage.setItem("Header",inp.value);
 }

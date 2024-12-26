5d2fc2c665476016919b83728aa8c4db53e19cb7
Nicholas-py <nicholas.waslander@gmail.com>
2024-12-26 14:29:00 -0500

    Functioning input return

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
-if (document.getElementById("toptexty").innerText =="code.js") {
-    document.getElementById("toptexty").innerText = "number 2"
+
+var start, inp, strhihi;
+
+function setup() {
+    start = document.getElementById("toptexty");
+    inp = document.getElementById("first");
+    strhihi = "that was a bad idea";
 }
-document.getElementById("toptexty").innerText = "code.js"
 
 function incodefunc() {
-    document.getElementById("toptexty").innerText = "beep"
+    if (start == null){
+        document.getElementById("toptexty").innerHTML = strhihi+"97";
+}
+else {
+    document.getElementById("toptexty").innerHTML = "lolfail";
+}
+    var oldtext = start.ariaAtomic +"no errors";
+    document.getElementById("toptexty").innerHTML = oldtext;
+    start.innerHTML = "burp";
+    start.innerHTML = inp.value;
 }

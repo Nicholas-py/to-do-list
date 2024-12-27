7a81b523efa01c0797a4bb195d279da90bb47b7b
Nicholas-py <nicholas.waslander@gmail.com>
2024-12-27 15:51:27 -0500

    Tables!

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 const separator = "@#*&^";
 
-var start, inp;
+var start, inp, debug;
+
+const pageitems = [];
 
 function isblank(str) {
     return Boolean(!str.trim());
@@ -25,11 +27,44 @@ class ListItem {
         str += separator + this.#category;
         return str;
     }
+
+    static fromstring(string) {
+        var items = string.split(separator);
+        return ListItem(items[0],items[1],items[2])
+    }
+}
+
+function save() {
+    for (let i = 0; i < pageitems.length; i++) {
+        localStorage.setItem("item"+String(i),pageitems[i].stringdata)
+    }
+    localStorage.setItem("item"+String(pageitems.length), "end")
+}
+
+function load() {
+    var counter = 0
+    while (true) {
+        var item = localStorage.getItem("item"+String(counter))
+        if (isblank(item) || item == "end") {
+            return;
+        }
+        pageitems[counter] = ListItem.fromstring(item);
+        counter++;
+    }
+}
+
+function edit() {
+
+}
+
+function currenttime() {
+    return Date(Date.now());
 }
 
 function setup() {
     start = document.getElementById("toptexty");
-    inp = document.getElementById("first");
+    inp = document.getElementById("test");
+    debug = document.getElementById("debug")
     start.innerText = localStorage.getItem("Header");
 }
 
